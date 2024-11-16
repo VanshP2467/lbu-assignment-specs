466b5ef6b5efb1b17ef279f197f45b56db6dee5c
VanshP2467 <V.Panchalwar2647@student.leedsbeckett.ac.uk>
2024-11-16 20:02:51 +0000

    Added basic error handling for input args

--- Project-1/cli.py
+++ Project-1/cli.py
@@ -1,49 +1,66 @@
-import argparse
-from tabulate import tabulate
+if __name__ == "__main__":
+    import argparse
 
-parser = argparse.ArgumentParser(
-    prog="Grand Prix Timings Board",
-    description="A program that visualizes timecodes from the Grand Prix",
-)
+    parser = argparse.ArgumentParser(
+        prog="Grand Prix Timings Board",
+        description="A program that visualizes timecodes from the Grand Prix",
+    )
 
-parser.add_argument(
-    "-filename", help="Path to the timecodes file you want to parse", type=str
-)
+    parser.add_argument(
+        "-filename",
+        help="Path to the timecodes file you want to parse",
+        type=str,
+        nargs="?",
+    )
 
-args = parser.parse_args()
+    args = parser.parse_args()
+
+    if args.filename is None:
+        print("Please specify a file")
+        parser.print_help()
+        exit(-1)
+
+race_name = ""
 
 
 def parse(filepath: str):
     lap_times = {}
     name = []
     value = []
-    race = ""
 
     # open file and fill the variables via python slicing
+    try:
+        open(file=filepath, mode="r")
+        print("Reading file")
+
+    except FileNotFoundError:
+        print("File not found. Please check the path and try again")
+
     with open(file=filepath, mode="r") as file:
         for index, lines in enumerate(file):
             strip = lines.strip()
             if index == 0:
-                race = lines
+                race_name = lines
             else:
                 name.append(strip[:3])
                 value.append(strip[3:])
 
-    value = list(map(float, value))
+        value = list(map(float, value))
 
-    for key, value in zip(name, value):
-        if key not in lap_times:
-            lap_times[key] = []
-        lap_times[key].append(value)
+        for key, value in zip(name, value):
+            if key not in lap_times:
+                lap_times[key] = []
+            lap_times[key].append(value)
 
-    return lap_times
+        return lap_times
 
 
-myfile = parse(R"D:\Scripts\uni\lbu-assignment-specs\Project-1\lap_times_1.txt")
+# myfile = parse(R"D:\Scripts\uni\lbu-assignment-specs\Project-1\lap_times_1.txt")
+myfile = parse(args.filename)
+# tabulate = print(tabulate(myfile, headers="keys", tablefmt="grid", numalign="left"))
 
-tabulate = print(tabulate(myfile, headers="keys", tablefmt="grid", numalign="left"))
 
 sorted_values = []
 for keys, values in myfile.items():
     sorted_values = values.sort()
-    print(f"Fastest for {keys} is {values[0]}")
+    print(f"Fastest lap time for {keys} is {values[0]}")
