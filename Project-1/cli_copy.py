766ccc29e591b742f0582779fc90236d63730cb5
VanshP2467 <V.Panchalwar2647@student.leedsbeckett.ac.uk>
2024-11-16 23:45:24 +0000

    testing out rich for making the tables new file:   Project-1/cli_copy.py

---
+++ Project-1/cli_copy.py
@@ -0,0 +1,89 @@
+from rich.console import Console
+from rich.table import Table
+from rich import box
+from rich.style import Style
+from tabulate import tabulate
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        prog="Grand Prix Timings Board",
+        description="A program that visualizes timecodes from the Grand Prix",
+    )
+
+    parser.add_argument(
+        "-filename",
+        help="Path to the timecodes file you want to parse",
+        type=str,
+        nargs="?",
+    )
+
+    args = parser.parse_args()
+
+    if args.filename is None:
+        print("Please specify a file")
+        parser.print_help()
+        exit(-1)
+
+
+def parse(filepath: str):
+    lap_times = {}
+    name = []
+    value = []
+    global race_name
+    # open file and fill the variables via python slicing
+    try:
+        open(file=filepath, mode="r")
+        print("Reading file")
+
+    except FileNotFoundError:
+        print("File not found. Please check the path and try again")
+        exit()
+
+    with open(file=filepath, mode="r") as file:
+        for index, lines in enumerate(file):
+            strip = lines.strip()
+            if index == 0:
+                race_name = lines
+            else:
+                name.append(strip[:3])
+                value.append(strip[3:])
+
+        value = list(map(float, value))
+
+        for key, value in zip(name, value):
+            if key not in lap_times:
+                lap_times[key] = []
+            lap_times[key].append(value)
+
+        return lap_times
+
+
+# myfile = parse(R"D:\Scripts\uni\lbu-assignment-specs\Project-1\lap_times_1.txt")
+myfile = parse(args.filename)
+# tabulate = print(tabulate(myfile, headers="keys", tablefmt="grid", numalign="left"))
+
+console = Console()
+
+table = Table(title=f"{race_name}", box=box.DOUBLE_EDGE)
+
+for keys, value in myfile.items():
+    table.add_column(f"{keys}")
+
+
+max_rows = max(len(value) for value in myfile.values())
+
+for i in range(max_rows):
+    row = []
+    for values in myfile.values():
+        # Add value if it exists; otherwise, add an empty string
+        row.append(str(values[i]) if i < len(values) else "")
+    table.add_row(*row)
+
+
+console.print(table)
+# sorted_values = []
+# for keys, values in myfile.items():
+#     sorted_values = values.sort()
+#     print(f"Fastest lap time for {keys} is {values[0]}")
